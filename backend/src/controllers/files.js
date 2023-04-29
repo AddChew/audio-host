d02600539566d6d6f36ea21e9d7d51b482902e2d
AddChew <addison_chew@outlook.com>
2023-04-29 17:46:29 +0800

    add files controller

---
+++ backend/src/controllers/files.js
@@ -0,0 +1,52 @@
+const File = require('../models/file')
+
+// get all files owned by current user
+exports.getFiles = (req, res, next) => {
+    File.findAll({
+        attributes: {
+            exclude: ['content']
+        }, 
+        where: {
+            ownerUuid: req.user.uuid
+        }})
+        .then(files => {
+            res.status(200).json({ files: files })
+        })
+        .catch(err => console.log(err))
+}
+
+// get file by uuid, owned by current user
+exports.getFile = (req, res, next) => {
+    File.findOne({
+        where: {
+            uuid: req.params.fileUuid,
+            ownerUuid: req.user.uuid
+        }
+    })
+    .then(file => {
+        if (file) {
+            return res.status(200).json({ file: file })
+        }
+        res.status(404).json({ message: 'File not found!' })
+    })
+    .catch(err => console.log(err))
+}
+
+// create file
+exports.createFile = (req, res, next) => {
+    File.create({
+        filename: req.body.filename,
+        description: req.body.description,
+        category: req.body.category,
+        content: req.body.content,
+        ownerUuid: req.user.userUuid
+    })
+    .then(file => {
+        console.log(`Created file ${file.filename}`)
+        res.status(201).json({
+            message: `File ${file.filename} created successfully!`,
+            file: file
+        })
+    })
+    .catch(err => console.log(err))
+}
