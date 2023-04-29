5742e5bbb8d13b15252fde483fbad7be6a26f965
AddChew <addison_chew@outlook.com>
2023-04-29 17:42:29 +0800

    update user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,21 +2,33 @@ const { DataTypes } = require('sequelize')
 const db = require('../utils/database')
 const File = require('../models/file')
 
-const User = db.define('User', {
-    uuid: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false
-    },
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false
+const User = db.define('User', 
+    {
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        }
+    }, 
+    {
+        hooks: {
+            afterCreate: (record) => {
+                delete record.dataValues.password
+            },
+            afterUpdate: (record) => {
+                delete record.dataValues.password
+            }
+        }
     }
-})
+)
 
 User.hasMany(File, {
     foreignKey: 'ownerUuid'
