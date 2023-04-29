3695e2d227f251316b64dfb1248a8e72fb98779b
AddChew <addison_chew@outlook.com>
2023-04-29 17:45:06 +0800

    update file model

--- backend/src/models/file.js
+++ backend/src/models/file.js
@@ -1,17 +1,29 @@
 const { DataTypes } = require('sequelize')
 const db = require('../utils/database')
 
-const File = db.define('File', {
-    uuid: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false
+const File = db.define('File', 
+    {
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        filename: DataTypes.STRING,
+        description: DataTypes.STRING,
+        category: DataTypes.STRING,
+        content: DataTypes.BLOB
     },
-    filename: DataTypes.STRING,
-    description: DataTypes.STRING,
-    category: DataTypes.STRING,
-    content: DataTypes.BLOB
-})
+    {
+        hooks: {
+            afterCreate: (record) => {
+                delete record.dataValues.content
+            },
+            afterUpdate: (record) => {
+                delete record.dataValues.content
+            }
+        }
+    }
+)
 
 module.exports = File
