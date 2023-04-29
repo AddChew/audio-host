f6a25d9910086888b99f7187b84e2d4849e5b7f6
AddChew <addison_chew@outlook.com>
2023-04-29 13:21:15 +0800

    add user routes

---
+++ backend/src/routes/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const controller = require('../controllers/users')
+
+router.get('/', controller.getUsers) // GET /users
+router.post('/', controller.createUser) // POST /users
+router.get('/:userUuid', controller.getUser) // GET /users/:userUuid
+router.put('/:userUuid', controller.updateUser) // PUT /users/:userUuid
+router.delete('/:userUuid', controller.deleteUser) // DELETE /users/:userUuid
