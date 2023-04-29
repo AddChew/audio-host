1b4b8c7e91f63747fbe78c70f238e943c493fa83
AddChew <addison_chew@outlook.com>
2023-04-29 19:48:59 +0800

    switch to async await syntax for files

--- backend/src/controllers/files.js
+++ backend/src/controllers/files.js
@@ -1,52 +1,60 @@
-const File = require('../models/file') // TODO: async, await everything instead of then catch
+const File = require('../models/file')
 
 // get all files owned by current user
-exports.getFiles = (req, res, next) => {
-    File.findAll({
-        attributes: {
-            exclude: ['content']
-        }, 
-        where: {
-            ownerUuid: req.user.uuid
-        }})
-        .then(files => {
-            res.status(200).json({ files: files })
+exports.getFiles = async (req, res, next) => {
+    try {
+        const files = await File.findAll({
+            attributes: {
+                exclude: ['content']
+            },
+            where: {
+                ownerUuid: req.user.uuid
+            }
         })
-        .catch(err => console.log(err))
+        res.status(200).json({ files: files })
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 // get file by uuid, owned by current user
-exports.getFile = (req, res, next) => {
-    File.findOne({
-        where: {
-            uuid: req.params.fileUuid,
-            ownerUuid: req.user.uuid
-        }
-    })
-    .then(file => {
+exports.getFile = async (req, res, next) => {
+    try {
+        const file = await File.findOne({
+            where: {
+                uuid: req.params.fileUuid,
+                ownerUuid: req.user.uuid
+            }
+        })
         if (file) {
             return res.status(200).json({ file: file })
         }
         res.status(404).json({ message: 'File not found!' })
-    })
-    .catch(err => console.log(err))
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 // create file
-exports.createFile = (req, res, next) => {
-    File.create({ // TODO: const { filename, description , ... } = req.body
-        filename: req.body.filename,
-        description: req.body.description,
-        category: req.body.category,
-        content: req.body.content,
-        ownerUuid: req.user.userUuid
-    })
-    .then(file => {
+exports.createFile = async (req, res, next) => {
+    try {
+        const { filename, description, category, content } = req.body
+        const file = await File.create({
+            filename: filename,
+            description: description,
+            category: category,
+            content: content,
+            ownerUuid: req.user.userUuid
+        })
         console.log(`Created file ${file.filename}`)
         res.status(201).json({
             message: `File ${file.filename} created successfully!`,
             file: file
-        })
-    })
-    .catch(err => console.log(err))
+        })       
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
