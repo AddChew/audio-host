7977bcea45ebd9e61e782b8e3914b9aeb0c883e0
AddChew <addison_chew@outlook.com>
2023-04-29 17:48:48 +0800

    add files routes

---
+++ backend/src/routes/files.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const controller = require('../controllers/files')
+
+router.get('/', controller.getFiles) // GET /files
+router.post('/', controller.createFile) // POST /files
+router.get('/:fileUuid', controller.getFile) // GET /files/:fileUuid
