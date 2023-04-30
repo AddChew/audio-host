06208556288eab30237d82caa7687becfdd9bb79
AddChew <addison_chew@outlook.com>
2023-05-01 00:27:01 +0800

    restore routes

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 const controller = require('../controllers/users')
 const { checkAuthenticated, checkIsAdmin } = require('../controllers/auth')
 
-router.get('/', controller.getUsers) // GET /users
-router.post('/', controller.createUser) // POST /users
-router.get('/:userid', controller.getUser) // GET /users/:userid
-router.put('/:userid', controller.updateUser) // PUT /users/:userid
-router.delete('/:userid', controller.deleteUser) // DELETE /users/:userid
+router.get('/', checkAuthenticated, controller.getUsers) // GET /users
+router.post('/', checkAuthenticated, controller.createUser) // POST /users
+router.get('/:userid', checkAuthenticated, controller.getUser) // GET /users/:userid
+router.put('/:userid', checkAuthenticated, controller.updateUser) // PUT /users/:userid
+router.delete('/:userid', checkAuthenticated, controller.deleteUser) // DELETE /users/:userid
 
 // router.get('/', checkAuthenticated, checkIsAdmin, controller.getUsers) // GET /users
 // router.post('/', checkAuthenticated, checkIsAdmin, controller.createUser) // POST /users
