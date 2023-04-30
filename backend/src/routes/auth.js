383c4b52b56b669a6369dcc85f29263105ce70bd
AddChew <addison_chew@outlook.com>
2023-04-30 18:46:05 +0800

    fix logout in backend

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,6 +3,6 @@ const controller = require('../controllers/auth')
 
 router.post('/register', controller.checkNotAuthenticated, controller.registerUser) // POST /auth/register
 router.post('/login', controller.checkNotAuthenticated, controller.loginUser) // POST /auth/login
-router.delete('/logout', controller.checkAuthenticated, controller.logoutUser) // DELETE /auth/logout
+router.delete('/logout', controller.logoutUser) // DELETE /auth/logout
 
 module.exports = router
