3086722db9e90d307578475749707abbb52a9663
AddChew <addison_chew@outlook.com>
2023-05-01 15:45:54 +0800

    fix rbac backend

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,16 +2,10 @@ const router = require('express').Router()
 const controller = require('../controllers/users')
 const { checkAuthenticated, checkIsAdmin } = require('../controllers/auth')
 
-router.get('/', checkAuthenticated, controller.getUsers) // GET /users
-router.post('/', checkAuthenticated, controller.createUser) // POST /users
-router.get('/:userid', checkAuthenticated, controller.getUser) // GET /users/:userid
-router.put('/:userid', checkAuthenticated, controller.updateUser) // PUT /users/:userid
-router.delete('/:userid', checkAuthenticated, controller.deleteUser) // DELETE /users/:userid
-
-// router.get('/', checkAuthenticated, checkIsAdmin, controller.getUsers) // GET /users
-// router.post('/', checkAuthenticated, checkIsAdmin, controller.createUser) // POST /users
-// router.get('/:userid', checkAuthenticated, checkIsAdmin, controller.getUser) // GET /users/:userid
-// router.put('/:userid', checkAuthenticated, checkIsAdmin, controller.updateUser) // PUT /users/:userid
-// router.delete('/:userid', checkAuthenticated, checkIsAdmin, controller.deleteUser) // DELETE /users/:userid
+router.get('/', checkAuthenticated, checkIsAdmin, controller.getUsers) // GET /users
+router.post('/', checkAuthenticated, checkIsAdmin, controller.createUser) // POST /users
+router.get('/:userid', checkAuthenticated, checkIsAdmin, controller.getUser) // GET /users/:userid
+router.put('/:userid', checkAuthenticated, checkIsAdmin, controller.updateUser) // PUT /users/:userid
+router.delete('/:userid', checkAuthenticated, checkIsAdmin, controller.deleteUser) // DELETE /users/:userid
 
 module.exports = router
