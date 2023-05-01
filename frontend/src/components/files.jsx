5879087a17b06e05be9c9d46711d6dbe2678d0ef
AddChew <addison_chew@outlook.com>
2023-05-01 09:59:51 +0800

    update upload input message

--- frontend/src/components/files.jsx
+++ frontend/src/components/files.jsx
@@ -22,10 +22,17 @@ export const FileList = () => (
     </List>
 )
 
+const FileInputMessage = () => (
+    <div>
+        <p>Drop a file to upload, or click to select it.</p>
+        <p style={{ fontSize: 12 }}>Max upload size: 25MB</p>
+    </div>
+)
+
 export const FileCreate = () => (
     <Create>
         <SimpleForm>
-            <FileInput source="content" accept="audio/*" maxSize={25000000}>
+            <FileInput source="content" accept="audio/*" maxSize={25000000} placeholder={ <FileInputMessage /> }>
                 <FileField source="src" title="title" />
             </FileInput>
             <TextInput source="filename" />
