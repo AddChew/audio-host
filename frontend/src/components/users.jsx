8143643ddd9cdb7d6ea8de3f47265c732511caa0
AddChew <addison_chew@outlook.com>
2023-05-02 00:40:30 +0800

    redirect to show after create user

--- frontend/src/components/users.jsx
+++ frontend/src/components/users.jsx
@@ -12,7 +12,9 @@ import {
     Create,
     useRecordContext,
     BooleanField,
-    required
+    required,
+    Show,
+    SimpleShowLayout
 } from 'react-admin'
 
 const UserTitle = () => {
@@ -50,4 +52,15 @@ export const UserCreate = () => (
             <BooleanInput source="isAdmin" validate={ required() } />
         </SimpleForm>
     </Create>
+)
+
+export const UserShow = () => (
+    <Show title={ <UserTitle />}>
+        <SimpleShowLayout>
+            <TextField source="username" />
+            <BooleanField source="isAdmin" />
+            <DateField source="createdAt" showTime />
+            <DateField source="updatedAt" showTime />
+        </SimpleShowLayout>
+    </Show>
 )
