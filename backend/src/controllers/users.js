381fa518357b55f4718c58342dd13c0e4429d451
AddChew <addison_chew@outlook.com>
2023-05-01 09:00:20 +0800

    update delete

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -88,7 +88,7 @@ exports.deleteUser = async (req, res, next) => {
         const user = await User.findByPk(req.params.userid)
         if (user) {
             await user.destroy()
-            return res.status(204).json({ message: 'User deleted' }) // TODO: might need to 200
+            return res.status(200).json({ message: 'User deleted', user: user })
         }
         return res.status(404).json({ message: 'User not found'})
     }
